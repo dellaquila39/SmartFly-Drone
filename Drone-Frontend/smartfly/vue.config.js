e90780416c8e63a77507f95ff14e9525da51ab89
agustin dell aquila <dellaquila39492276@hotmail.com>
2025-05-29 23:58:48 -0300

    Build exitoso - Listo para producción

--- Drone-Frontend/smartfly/vue.config.js
+++ Drone-Frontend/smartfly/vue.config.js
@@ -1,12 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
+  transpileDependencies: ['vuetify'],
   
-  // Configuración esencial para Netlify
   publicPath: '/',
+  
+  configureWebpack: {
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css'
+      })
+    ]
+  },
+  
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].title = "SmartFly Pro - Soluciones Aéreas";
