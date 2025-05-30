c4ed2f8e89adac743bc248cfd4730f3493d18ec0
agustin dell aquila <dellaquila39492276@hotmail.com>
2025-05-29 23:27:38 -0300

    Configuración para despliegue en Netlify

--- Drone-Frontend/smartfly/src/router/index.js
+++ Drone-Frontend/smartfly/src/router/index.js
@@ -13,14 +13,18 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
+  },
+  // Agrega esta ruta adicional para manejar errores 404
+  {
+    path: '*',
+    redirect: '/'
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history', 
+  base: process.env.BASE_URL,
   routes
 })
 
